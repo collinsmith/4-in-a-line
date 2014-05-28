15e6f690ffa60f22c93202eb3e0179aac3e0887a
Collin Smith <collinsmith@csupomona.edu>
2014-05-27 18:07:08 -0700

    Update Main.java

--- src/edu/csupomona/cs/cs420/project3/Main.java
+++ src/edu/csupomona/cs/cs420/project3/Main.java
@@ -1,152 +1,206 @@
 package edu.csupomona.cs.cs420.project3;
 
-public class Main {
-	public static void main(String[] args) {
-		State b = new State();
-		b = b.set(0, 0, State.O);
-		b = b.set(1, 1, State.X);
-		b = b.set(2, 2, State.O);
-		b = b.set(1, 3, State.X);
-		b = b.set(0, 4, State.O);
-		b = b.set(5, 0, State.X);
-		System.out.println(b);
-		System.out.println(b.get(1, 0));
+import java.util.Scanner;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main	{
+   private static List<Move> moves;
+
+	public static void main(String[]	args)	{
+      Main.moves = new ArrayList<>(64);
+      
+      final Scanner SCAN = new Scanner(System.in);
+
+      Move last = null;      
+      State current = new State();
+      while (moves.size() < 64) {
+         System.out.format("Turn %d:%n", moves.size()+1);
+         System.out.format("%s%n", current);
+         
+         if (last != null && State.isWinningMove(current, last.i, last.j)) {
+            System.out.format("Player %d has won!%n", (moves.size()&1) != 0 ? 1 : 2);
+            break;
+         } else {
+            displayMoves();
+         }
+         
+         String next = SCAN.nextLine();
+         int i = Character.isUpperCase(next.charAt(0)) ? next.charAt(0)-'A' : next.charAt(0)-'a';
+         int j = next.charAt(1)-'0'-1;
+         current = current.set(i, j, (moves.size()&1) == 0 ? State.O : State.X);
+
+         last = new Move(i, j);
+         moves.add(last);
+      }
 	}
+   
+   private static void displayMoves() {
+      if (moves == null) {
+         throw new IllegalStateException();
+      }
+      
+      System.out.format("Move List:%n");
+      System.out.format("    O  X%n");
+
+      Iterator<Move> it = moves.iterator();
+      for (int i = 0; true; i++) {
+         System.out.format("%2d.", i+1);
+         if (it.hasNext()) {
+            System.out.format(" %s", it.next());
+         } else {
+            break;
+         }
+         
+         if (it.hasNext()) {
+            System.out.format(" %s", it.next());
+         } else {
+            break;
+         }
+         
+         System.out.format("%n");
+      }
+   }
+
+   private static class Move {
+      final int i;
+      final int j;
+   
+      Move(int i, int j) {
+         this.i = i;
+         this.j = j;
+      }
+      
+      @Override
+      public String toString() {
+         return String.format("%c%d", 'A'+i, j+1);
+      }
+   }
 	
-	private static class State {
-		static final long O = 1L<<0;
-		static final long X = 1L<<1;
+	private static	class	State	{
+		static final int	O = 1<<0;
+		static final int	X = 1<<1;
 		
-		final long O_LOCS;
-		final long X_LOCS;
+		final	long O_LOCS;
+		final	long X_LOCS;
 		
 		State() {
-			this(0L, 0L);
+			this(0L,	0L);
 		}
 		
-		State(long oLocs, long xLocs) {
-			this.O_LOCS = oLocs;
-			this.X_LOCS = xLocs;
+		State(long oLocs,	long xLocs)	{
+			this.O_LOCS	= oLocs;
+			this.X_LOCS	= xLocs;
 		}
 		
-		long getShift(long i, long j) {
+		static int	getShift(int i, int j) {
 			return ((i<<3)+j);
 		}
 		
-		long get(long i, long j) {
-			long mod = 1L<<getShift(i, j);
-			long o = O_LOCS&mod;
-			long x = X_LOCS&mod;
-			if (o != 0) {
+		int get(int i, int j) {
+			long mod	= 1L<<getShift(i,	j);
+			long o =	O_LOCS&mod;
+			long x =	X_LOCS&mod;
+			if	(o	!=	0)	{
 				return O;
 			} else if (x != 0) {
 				return X;
 			} else {
-				return 0L;
+				return 0;
 			}
-			
-			//return (o != 0 ? 0b01 : 0)|(x != 0 ? 0b10 : 0);
 		}
 		
-		State set(long i, long j, long val) {
-			long shift = getShift(i, j);
-			long o = O_LOCS;
-			long x = X_LOCS;
-			if (val == O) {
-				o |= 1<<shift;
-			} else if (val == X) {
-				x |= 1<<shift;
+		State	set(int i,	int j, int val)	{
+			int shift = getShift(i, j);
+			long o =	O_LOCS;
+			long x =	X_LOCS;
+			if	(val == O) {
+				o |= 1L<<shift;
+			} else if (val	==	X)	{
+				x |= 1L<<shift;
 			} else {
-				throw new IllegalArgumentException();
+				throw	new IllegalArgumentException();
 			}
 			
 			return new State(o, x);
 		}
 		
+      static boolean isWinningMove(State s, int i, int j) {
+         int val = s.get(i, j);
+         if (val == O) {
+            int countRow = 0;
+            int countColumn = 0;
+            for (int x = 0; x < 8; x++) {
+               if (s.get(x, j) == O) {
+                  countRow++;
+                  if (countRow == 4) {
+                     return true;
+                  }
+               } else {
+                  countRow = 0;
+               }
+               
+               if (s.get(i, x) == O) {
+                  countColumn++;
+                  if (countColumn == 4) {
+                     return true;
+                  }
+               } else {
+                  countColumn = 0;
+               }
+            }
+            
+            return false;
+         } else if (val == X) {
+            int countRow = 0;
+            int countColumn = 0;
+            for (int x = 0; x < 8; x++) {
+               if (s.get(x, j) == X) {
+                  countRow++;
+                  if (countRow == 4) {
+                     return true;
+                  }
+               } else {
+                  countRow = 0;
+               }
+               
+               if (s.get(i, x) == X) {
+                  countColumn++;
+                  if (countColumn == 4) {
+                     return true;
+                  }
+               } else {
+                  countColumn = 0;
+               }
+            }
+            
+            return false;
+         } else {
+            return false;
+         }
+      }
+      
 		@Override
 		public String toString() {
 			StringBuilder sb = new StringBuilder(172);
 			sb.append("  1 2 3 4 5 6 7 8 \n");
 			
 			long loc;
-			for (int i = 0; i < 8; i++) {
+			for (int	i = 0; i	< 8; i++) {
 				sb.append(((char)('A'+i)));
 				sb.append(' ');
 				
-				for (int j = 0; j < 8; j++) {
-					if (get(i, j) != 0) {
-						System.out.format("%d, %d%n", i, j);
-					}
-					
-					loc = get(i, j);
-					if (loc == 0) {
+				for (int	j = 0; j	< 8; j++) {
+					loc =	get(i, j);
+					if	(loc == 0) {
 						sb.append("- ");
-					} else if (loc == O) {
+					} else if (loc	==	O)	{
 						sb.append("O ");
-					} else if (loc == X) {
+					} else if (loc	==	X)	{
 						sb.append("X ");
 					} else {
-						throw new IllegalStateException();
-					}
-				}
-				
-				sb.append('\n');
-			}
-
-			sb.deleteCharAt(sb.length()-1);
-			return sb.toString();
-		}
-	}
-	
-	private static class Board {
-		static final byte O = 1<<0;
-		static final byte X = 1<<1;
-		
-		final long BOARD_O, BOARD_X;
-		
-		Board(long board_o, long board_x) {
-			this.BOARD_O = board_o;
-			this.BOARD_X = board_x;
-		}
-		
-		byte get(int i, int j) {
-			long mod = 1L<<((i<<3)+j);
-			long o = BOARD_O&mod;
-			long x = BOARD_X&mod;
-			return (byte)((o != 0 ? 0b01 : 0)|(x != 0 ? 0b10 : 0));
-		}
-		
-		Board set(int i, int j, byte val) {
-			int mod = ((i<<3)+j);
-			long o = (BOARD_O&~(1L<<mod))|(((val&Board.O)>>0)<<mod);
-			long x = (BOARD_X&~(1L<<mod))|(((val&Board.X)>>1)<<mod);
-			return new Board(o, x);
-		}
-		
-		@Override
-		public String toString() {
-			StringBuilder sb = new StringBuilder(172);
-			sb.append("  1 2 3 4 5 6 7 8 \n");
-			for (int i = 0; i < 8; i++) {
-				sb.append(((char)('A'+i)));
-				sb.append(' ');
-				
-				for (int j = 0; j < 8; j++) {
-					if (get(i, j) != 0) {
-						System.out.format("%d, %d%n", i, j);
-					}
-					switch (get(i, j)) {
-						case 0b00:
-							sb.append("- ");
-							break;
-						case 0b01:
-							sb.append("O ");
-							break;
-						case 0b10:
-							sb.append("X ");
-							break;
-						default:
-							throw new IllegalStateException(String.format("Location (%d, %d) has an invalid state (%d)", i, j, get(i, j)));
+						throw	new IllegalStateException();
 					}
 				}
 				
